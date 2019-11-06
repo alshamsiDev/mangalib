6eda5f10256bb8a987ce496a212fadccc9684057
Saud Alshamsi <23ud92@gmail.com>
2019-11-06 16:14:11 +0300

    Updates

---
+++ precache-manifest.f8a8b0af3f1c9a65debdcc47f7f34426.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6398247b9562e0e6078ab1a78f24fd9a",
+    "url": "/mangalib/index.html"
+  },
+  {
+    "revision": "04bb9d6cf2ffb78253d0",
+    "url": "/mangalib/static/css/main.721eac68.chunk.css"
+  },
+  {
+    "revision": "d20ccf241cd3b96e6ac1",
+    "url": "/mangalib/static/js/2.62b15246.chunk.js"
+  },
+  {
+    "revision": "04bb9d6cf2ffb78253d0",
+    "url": "/mangalib/static/js/main.ac73563c.chunk.js"
+  },
+  {
+    "revision": "d7b2c783a0bad14eba4b",
+    "url": "/mangalib/static/js/runtime-main.9f84db86.js"
+  }
+]);
