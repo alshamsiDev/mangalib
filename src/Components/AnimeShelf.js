2485e4c661f34607672643e6731a31d285daad2d
Saud Alshamsi <23ud92@gmail.com>
2019-11-06 11:28:30 +0300

    Done with cart

--- src/Components/AnimeShelf.js
+++ src/Components/AnimeShelf.js
@@ -3,7 +3,8 @@ export default class AnimeShelf extends Component {
     constructor(props) {
         super(props);
         this.state = ({
-            count: 0
+            count: 0,
+            details: null
         })
     }
     handleClick = (e) => {
@@ -11,7 +12,6 @@ export default class AnimeShelf extends Component {
         this.setState(prevState => {
             count: prevState.count++
         });
-
         // count = count++;
         this.props.addNewAnime(this.props.anime);
     }
@@ -20,25 +20,30 @@ export default class AnimeShelf extends Component {
         this.props.removeAnime(this.props.anime);
     }
     showDetails = (anime) => {
-        return (
-            <div>
+        this.setState({
+            details: <div>
                 <h1>{anime.title}</h1>
                 <h2>{anime.episodes}</h2>
-            </div>)
+            </div>
+        })
+
 
     }
     render() {
-        console.log(this.state.count);
-        
+        const counter = this.props.counter.filter(item => {
+            return item.title === this.props.anime.title;
+        })
+
         return (
             <div>
                 <img src={this.props.anime.image_url} alt={this.props.anime.title} />
-                <span>{this.props.counter}</span>
+                <span>{counter.length}</span>
                 <div>
                     <h1>{this.props.anime.title}</h1>
                 </div>
                 <button onClick={this.handleClick}>Quick add to scarts</button>
                 <button onClick={() => this.showDetails(this.props.anime)}>More details</button>
+                {this.state.details}
             </div>
         )
     }
